4843d2af824e096066fe3fa16e773e6181ece7c1
João Neto <joaoneto@Joaos-MacBook-Air.local>
2023-08-17 23:01:37 -0300

    add test e2e duplicate create category

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -22,10 +22,11 @@ describe("Create Category controller", () => {
              VALUES('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
         `)     
     })
-
-    afterAll( async () => {
+    
+    afterAll(async () => {
         await connection.dropDatabase();
         await connection.close()
+        console.log('FIM Category');
     })
     
     it("should be able to create a new category", async () => {
@@ -47,5 +48,26 @@ describe("Create Category controller", () => {
             })
 
         expect(response.status).toBe(201);
+    })
+
+    it("should not be able to create a duplicate category", async () => {
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@rentx.com.br",
+            password: "admin"
+        });
+
+        const { token } = responseToken.body
+
+        const response = await request(app)
+            .post("/categories")
+            .set({
+                Authorization: `Bearer ${token}` 
+            })
+            .send({
+                name: "Category supertest",
+                description: "Category description supertest"
+            })
+
+        expect(response.status).toBe(400);
     })
 })
