ce099d0856f66c9899f064385969f320f2fb33f7
João Neto <joaoneto@Joaos-MacBook-Air.local>
2023-09-05 21:17:12 -0300

    change readme

--- README.md
+++ README.md
@@ -1,60 +1,97 @@
-# Levantamento de Requisitos
+# study-rentcar-api
 
-## Cadastro de carro
+This is a study project of a car rental api
 
-**RF**
+## 📘 Tech Stack
+
+**Server:**
+
+- Typescript
+- Node
+- Express
+- Docker
+- Jest
+
+## ⭐ Installation
+
+Install my-project with npm
+
+```bash
+  docker-compose up --build
+```
+
+## ✅ Running Tests
+
+To run tests, run the following command
+
+```bash
+  npm run test
+```
+
+## 📌 Requirements
+
+**Cadastro de carro**
+
+Requisito Funcional
+
 - Deve ser possivel cadastrar um novo carro
 - Deve ser possivel listar todas as categorias
 
-**RN**
-- Não deve ser possivel cadastra um carro com uma placa já existente 
+Regra de negocio
+
+- Não deve ser possivel cadastra um carro com uma placa já existente
 - O carro deve ser cadastrado, por padrão, com disponibilidade
 - O usuário repsponsável pelo cadastro deve ser um usuário administrador
 
-## Listagen de carros
+**Listagen de carros**
 
-**RF**
-- Deve ser possivel listar todos os carros disponíveis 
+Requisito Funcional
+
+- Deve ser possivel listar todos os carros disponíveis
 - Deve ser possivel listar todos os carros disponíveis pelo nome da cateogira
-- Deve ser possivel listar todos os carros disponíveis pelo nome da marca 
+- Deve ser possivel listar todos os carros disponíveis pelo nome da marca
 - Deve ser possivel listar todos os carros disponíveis pelo nome do carro
 
-**RN**
+Regra de negocio
+
 - O usuario não precisa estar autenticado no sistema
 
-## Cadastro de especificação do carro
+**Cadastro de especificação do carro**
 
-**RF**
+Requisito Funcional
+
 - Deve ser possivel cadastrar um especificação para um carro
 
-**RN**
+Regra de negocio
+
 - Não deve ser possivel cadastrar uma especificação para um carro não cadastrado
 - Não deve ser possivel cadastrar uma especificação já existente para o mesmo carro
 - O usuario responsavel pelo cadastro deve ser um usuário administrador
 
-## Cadastro de imagem do carro
+**Cadastro de imagem do carro**
 
-**RF**
+Requisito Funcional
+
 - Deve ser possivel cadastrar a imagem do carro
 - Deve ser possivel listar todos os carros
 
-**RNF**
+Requisito Não funciona
+
 - Utilizar o multer para upload dos arquivos
 
-**RN**
+Regra de negocio
+
 - O usuario deve poder cadastrar mais de uma imagem para o mesmo carro
-- O usario responsavel pelo cadastro deve ser um usuario administrador 
+- O usario responsavel pelo cadastro deve ser um usuario administrador
 
-## Aluguel de carro 
+**Aluguel de carro**
 
-**RF**
+Requisito Funcional
+
 - Deve ser possivel cadastrar um aluguel
 
-**RNF**
+Regra de negocio
+
 - O aluguel deve ter duração mínima de 24 horas
 - Não deve ser possivel cadastrar um novo aluguel caso já exista um aberto para o mesmo usuario
 - Não deve ser possivel cadastrar um novo aluguel caso já exista um aberto para o mesmo carro
-
-
-
-
--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,73 +1,72 @@
-import request from "supertest";
-import { v4 as uuidV4 } from "uuid"
 import { hash } from "bcrypt";
+import request from "supertest";
 import { Connection, createConnection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
 
 import { app } from "@shared/infra/http/app";
 
-let connection: Connection
+let connection: Connection;
 
 describe("Create Category controller", () => {
-    
-    beforeAll(async () => {
-        connection = await createConnection();
-        
-        await connection.runMigrations();
-        
-        const id = uuidV4();
-        const password = await hash("admin",8)
-     
-        await connection.query(`
+  beforeAll(async () => {
+    connection = await createConnection();
+
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const password = await hash("admin", 8);
+
+    await connection.query(`
              INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
              VALUES('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
-        `)     
-    })
-    
-    afterAll(async () => {
-        await connection.dropDatabase();
-        await connection.close()
-        console.log('FIM Category');
-    })
-    
-    it("should be able to create a new category", async () => {
-        const responseToken = await request(app).post("/sessions").send({
-            email: "admin@rentx.com.br",
-            password: "admin"
-        });
+        `);
+  });
 
-        const { token } = responseToken.body
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+    console.log("FIM Category");
+  });
 
-        const response = await request(app)
-            .post("/categories")
-            .set({
-                Authorization: `Bearer ${token}` 
-            })
-            .send({
-                name: "Category supertest",
-                description: "Category description supertest"
-            })
+  it("should be able to create a new category", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin",
+    });
 
-        expect(response.status).toBe(201);
-    })
+    const { token } = responseToken.body;
 
-    it("should not be able to create a duplicate category", async () => {
-        const responseToken = await request(app).post("/sessions").send({
-            email: "admin@rentx.com.br",
-            password: "admin"
-        });
+    const response = await request(app)
+      .post("/categories")
+      .set({
+        Authorization: `Bearer ${token}`,
+      })
+      .send({
+        name: "Category supertest",
+        description: "Category description supertest",
+      });
 
-        const { token } = responseToken.body
+    expect(response.status).toBe(201);
+  });
 
-        const response = await request(app)
-            .post("/categories")
-            .set({
-                Authorization: `Bearer ${token}` 
-            })
-            .send({
-                name: "Category supertest",
-                description: "Category description supertest"
-            })
+  it("should not be able to create a duplicate category", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin",
+    });
 
-        expect(response.status).toBe(400);
-    })
-})
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post("/categories")
+      .set({
+        Authorization: `Bearer ${token}`,
+      })
+      .send({
+        name: "Category supertest",
+        description: "Category description supertest",
+      });
+
+    expect(response.status).toBe(400);
+  });
+});
